ef808f5e006b92a3ac8d4ee5a0c2ccd91dac1d4c
Dasha Nos <dasha.nos91@gmail.com>
2019-10-26 09:55:13 +0300

    Initial commit

---
+++ src/test/java/ru/netology/CallbackTest.java
@@ -0,0 +1,33 @@
+package ru.netology;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class CallbackTest {
+
+    private WebDriver driver;
+
+    @BeforeAll
+    static void setUpAll() {
+        System.setProperty("webdriver.chrome.driver", "C:\\tmp\\chromedriver.exe");
+    }
+
+    @BeforeEach
+    void setUp() {
+        driver = new ChromeDriver();
+    }
+    @AfterEach
+    void tearDown() {
+        driver.quit();
+        driver = null;
+    }
+    @Test
+    void shouldTestSomething() {
+        throw new UnsupportedOperationException();
+    }
+
+}
