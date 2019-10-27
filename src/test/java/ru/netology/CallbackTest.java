f9c26847c0b0e9de1b4a264a8854342658c884c3
Dasha Nos <dasha.nos91@gmail.com>
2019-10-27 15:17:55 +0300

    sleep() removed

--- src/test/java/ru/netology/CallbackTest.java
+++ src/test/java/ru/netology/CallbackTest.java
@@ -95,13 +95,4 @@ public class CallbackTest {
         $(By.tagName("button")).click();
         $("[data-test-id=agreement]").shouldHave(cssClass("input_invalid"));
     }
-
-    void sleep(int sec) {
-        try {
-            Thread.sleep(sec * 1000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-    }
-
 }
